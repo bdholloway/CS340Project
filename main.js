cf1a27748264913131116720a9ff091efe44df30
Michael Payne <paynemi@flip1.engr.oregonstate.edu>
2020-05-29 14:55:39 -0700

    trying again to add stuff

--- main.js
+++ main.js
@@ -1,81 +1,128 @@
-/*
-    Uses express, dbcon for database connection, body parser to parse form data
-    handlebars for HTML templates
-*/
-
-var express = require('express');
-var mysql = require('./dbcon.js');
-var bodyParser = require('body-parser');
-var path = require('path');
-
-
+var express = require("express");
+var mysql = require("./dbcon.js");
+var bodyParser = require("body-parser");
+var path = require("path");
+var session = require("express-session");
 var app = express();
-var handlebars = require('express-handlebars');
+var handlebars = require("express-handlebars");
+app.use(session({ secret: "super secret" }));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+const bcrypt = require("bcrypt");
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("mysql", mysql);
+const saltRounds = bcrypt.genSaltSync(10);
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('mysql', mysql);
-
-app.get('/', function(req, res){
-  console.log('Inside login page');
-  res.render('Login');
+app.get("/", (req, res) => {
+	res.render("Login");
 });
 
-app.get('/Register', (req, res) => {
-  res.render('Register');
+app.post("/login", (req, res, next) => {
+	//console.log(req.user.name);
+	if (req.body.userType == "Member") {
+		mysql.pool.query(
+			"SELECT * from Member where username = ?;",
+			[req.body.username],
+			function (error, rows) {
+				if (error) {
+					console.log(err);
+				} else if (rows.length) {
+					bcrypt.compare(req.body.password, rows[0].hashedPassword, function (
+						err,
+						blah
+					) {
+						if (blah) {
+							console.log("WE DID IT!");
+							req.session.username = rows[0].username;
+							req.session.memberID = rows[0].memberID;
+							req.session.userType = req.body.userType;
+							console.log(req.session.userType);
+							//console.log(req.session.username);
+							//console.log(req.session.memberID);
+							req.session.save();
+							res.render("Register");
+						} else {
+							console.log("Passwords don't match");
+						}
+					});
+				} else {
+					console.log("Query didn't return any results.");
+				}
+			}
+		);
+	} else {
+		mysql.pool.query(
+			"SELECT * from Trainer where username = ?;",
+			[req.body.username],
+			function (error, rows) {
+				if (error) {
+					console.log(err);
+				} else if (rows.length) {
+					bcrypt.compare(req.body.password, rows[0].hashedPassword, function (
+						err,
+						blah
+					) {
+						if (blah) {
+							console.log("WE DID IT!");
+							req.session.username = rows[0].username;
+							req.session.trainerID = rows[0].trainerID;
+							req.session.userType = req.body.userType;
+							console.log(req.session.userType);
+							//console.log(req.session.username);
+							//console.log(req.session.memberID);
+							req.session.save();
+							res.render("Register");
+						} else {
+							console.log("Passwords don't match");
+						}
+					});
+				} else {
+					console.log("Query didn't return any results.");
+				}
+			}
+		);
+	}
 });
 
-//Route to users profile. 
-app.get('/Home', function(req, res){
-  var sessionQuery = 'SELECT * FROM WorkoutSession';
-  mysql.pool.query(sessionQuery, function(error, results, fields){
-    console.log(results);
-  res.render('Home', {
-    results: results
-  });
-});
-});
-
-app.get('/AddWorkout', (req, res) => {
-  res.render('AddWorkout');
-});
-
-app.get('/Rate', (req, res) => {
-  res.render('Rate');
+app.post("/register", (req, res, next) => {
+	bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+		mysql.pool.query(
+			"INSERT INTO Member (username, firstName, lastName, hashedPassword) VALUES (?, ?, ?, ?);",
+			[req.body.username, req.body.fname, req.body.lname, hash],
+			function (error, rows) {
+				if (error) {
+					console.log(hash);
+					console.log(err);
+				} else {
+					res.render("Login");
+				}
+			}
+		);
+	});
 });
 
-app.get('/index', (req, res) => {
-  res.render('Index');
+app.get("/register", (req, res) => {
+	res.render("Register");
 });
 
-app.get('/AddSession', (req, res) => {
-  var sessQuery = 'SELECT * FROM WorkoutSession';
-  mysql.pool.query(sessQuery, function(error, results, fields){
-    //console.log(results);
-  res.render('AddSession', {
-    results: results
-  });
-});
+app.use(function (req, res) {
+	res.status(404);
+	res.render("404");
 });
 
-
-
-
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render("500");
 });
 
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
-});
-
-app.set('port', 64813);
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://flipX.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+app.set("port", 7334);
+app.listen(app.get("port"), function () {
+	console.log(
+		"Express started on http://flipX.engr.oregonstate.edu:" +
+			app.get("port") +
+			"; press Ctrl-C to terminate."
+	);
 });
