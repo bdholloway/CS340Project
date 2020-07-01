510941c89c95cdda187d417b6f8a8e7357cc183e
bdholloway <58130509+bdholloway@users.noreply.github.com>
2020-07-01 12:45:43 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_hollowab',
-  password        : '6480',
-  database        : 'cs340_hollowab'
+  user            : '',
+  password        : '',
+  database        : ''
 });
 module.exports.pool = pool;
